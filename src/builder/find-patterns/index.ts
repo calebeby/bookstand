a6adbf0ebe1be4d4fff38f3e468b85ea7a809fa8
Caleb Eby <caleb.eby01@gmail.com>
2018-08-25 08:13:46 -0700

    Change findPatterns/config to point to md files instead of folders

--- src/builder/find-patterns/index.ts
+++ src/builder/find-patterns/index.ts
@@ -1,33 +1,18 @@
 import globby from 'globby'
 import { join } from 'path'
-import { readFile, exists } from 'fs'
+import { readFile } from 'fs'
 import { promisify } from 'util'
 
 const readFileAsync = promisify(readFile)
-const existsAsync = promisify(exists)
 
 const readConfig = async (cwd: string): Promise<BookstandConfig> =>
   JSON.parse(await readFileAsync(join(cwd, 'bookstand.json'), 'utf8'))
 
 type BookstandConfig = string[]
 
-const findPatternsMatchingConfig = async (
-  config: BookstandConfig,
-  cwd: string,
-) => {
-  const allDirectories = await globby(config, { cwd, onlyDirectories: true })
-  const matchingDirectories = await Promise.all(
-    allDirectories.map(async directory => {
-      const readmePath = join(directory, 'bookstand.md')
-      return (await existsAsync(join(cwd, readmePath))) ? readmePath : false
-    }),
-  )
-  return matchingDirectories.filter((p): p is string => p !== false)
-}
-
 const findPatterns = async (cwd = process.cwd()) => {
   const config = await readConfig(cwd)
-  return findPatternsMatchingConfig(config, cwd)
+  return globby(config, { cwd })
 }
 
 export default findPatterns
