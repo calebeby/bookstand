9b61acba8478652546a2d86e43bb59dcf94c8c7b
Caleb Eby <caleb.eby01@gmail.com>
2018-08-24 21:47:16 -0700

    Add generateModule

--- src/builder/generate-module/index.test.ts
+++ src/builder/generate-module/index.test.ts
@@ -1,5 +1,18 @@
 import generateModule from '.'
 
 test('generateModule', () => {
-  expect(generateModule()).toMatchInlineSnapshot(`undefined`)
+  expect(generateModule(['/foo/bar/baz', '/asdf/asdfljk']))
+    .toMatchInlineSnapshot(`
+"
+import { h, render } from 'preact'
+import Bookstand from './bookstand'
+
+import FooBarBaz from '/foo/bar/baz'
+import AsdfAsdfljk from '/asdf/asdfljk'
+
+const patterns = [FooBarBaz,AsdfAsdfljk]
+
+render(<Bookstand patterns={patterns} />, document.body)
+"
+`)
 })
--- src/builder/generate-module/index.ts
+++ src/builder/generate-module/index.ts
@@ -1,3 +1,18 @@
-const generateModule = () => {}
+import moduleToVariable from '../module-to-variable'
+
+const im = (p: string) => `import ${moduleToVariable(p)} from '${p}'`
+
+const generateModule = (patterns: string[]) => {
+  return `
+import { h, render } from 'preact'
+import Bookstand from './bookstand'
+
+${patterns.map(im).join('\n')}
+
+const patterns = [${patterns.map(p => moduleToVariable(p)).join(',')}]
+
+render(<Bookstand patterns={patterns} />, document.body)
+`
+}
 
 export default generateModule
